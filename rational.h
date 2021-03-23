d66858890e837eac8df7a65228ea6b57dd57dd2f
eliav-code <51802186+eliav-code@users.noreply.github.com>
2021-03-23 19:47:30 +0200

    Update rational.h

--- rational.h
+++ rational.h
@@ -1,5 +1,5 @@
 #pragma once
-#pragma once
+//#pragma once
 class Rational
 {
 private:
@@ -9,25 +9,28 @@ private:
 public:
 	Rational(int x, int y);        //constructor
 	Rational(const Rational& v);                    //copy constructor                             
+	
 	void setDenominator(int);                       //function that sets the value of the denmoinator
-	int getDenominator();                           //function that returns the value of the denmoinator
 	void setNumerator(int);                         //function that sets the value of the Numerator
+
+	int getDenominator();                           //function that returns the value of the denmoinator
 	int getNumerator();                             //function that returns the value of the Numerator
+	
 	void print();                                   //print function
 	bool equal(Rational);                           //function that checks if the numbers are equal or not
 	Rational add(Rational);                         //dfunction that returns the sum of to rational numbers
 	Rational operator+(const Rational& r)const;   //function that retunrning the sum of two numbers
-	Rational operator-(Rational);
-	Rational operator*(Rational);
+	Rational operator-(const Rational& r) const;
+	Rational operator*(const Rational& r) const;
 	Rational operator/(const Rational&r)const;
 	Rational& operator++();		//prefix
 	Rational operator++(int);		//postfix
-	Rational operator--();          //prefix
+	Rational& operator--();          //prefix
 	Rational operator--(int);      //postfix
 	bool operator>=(const Rational& r)const;
 	bool operator<=(const Rational& r)const;
-	bool operator>(Rational);
-	bool operator<(Rational);
+	bool operator>(const Rational& r) const;
+	bool operator<(const Rational& r) const;
 	bool operator==(const Rational& r)const;
 	bool operator!=(const Rational& r)const;
 };
